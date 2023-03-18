ab65e33b5b5d48e856e42e3717b2e4464f36cd84
Noman5237 <anonyman637@gmail.com>
2023-03-18 18:11:42 +0600

    text widget implemented

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 
 import App from './app/app';
+import { store } from './app/store/store';
+import './styles.scss'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </Provider>,
+  </BrowserRouter>
 );
