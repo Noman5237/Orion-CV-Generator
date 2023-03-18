8ee18163a779308995d977b680c58a9881313eaa
MumtahinaRahman <vanallope2015@gmail.com>
2023-03-18 18:59:34 +0600

    navbar added. needs editing

---
+++ apps/frontend/src/app/Nav/Nav.tsx
@@ -0,0 +1,66 @@
+//<reference path="path/types.d.ts" />
+import React, { useState } from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps, MenuTheme } from 'antd';
+import { Menu, Switch } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key?: React.Key | null,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+  type?: 'group',
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem('Dashboard', 'sub1', <MailOutlined />),
+  getItem('Templates', 'sub2', <AppstoreOutlined />),
+  getItem('Create CV', 'sub4', <SettingOutlined />)
+];
+
+const App: React.FC = () => {
+  const [theme, setTheme] = useState<MenuTheme>('dark');
+  const [current, setCurrent] = useState('1');
+
+  const changeTheme = (value: boolean) => {
+    setTheme(value ? 'dark' : 'light');
+  };
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <>
+      <Switch
+        checked={theme === 'dark'}
+        onChange={changeTheme}
+        checkedChildren="Dark"
+        unCheckedChildren="Light"
+      />
+      <br />
+      <br />
+      <Menu
+        theme={theme}
+        onClick={onClick}
+        style={{ width: 256, height: "100%" }}
+        selectedKeys={[current]}
+        mode="inline"
+        items={items}
+      />
+    </>
+  );
+};
+
+export default App;
--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,44 +1,112 @@
 import { Route, Routes, Link } from 'react-router-dom';
-
 import styles from './app.module.scss';
-
 import { Home } from './home/home';
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
 
-export function App() {
+const App: React.FC = () => {
+	const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+	label: React.ReactNode,
+	key: React.Key,
+	icon?: React.ReactNode,
+	children?: MenuItem[],
+  ): MenuItem {
+	return {
+	  key,
+	  icon,
+	  children,
+	  label,
+	} as MenuItem;
+  }
+  
+  const items: MenuItem[] = [
+	getItem('Option 1', '1', <PieChartOutlined />),
+	getItem('Option 2', '2', <DesktopOutlined />),
+	getItem('User', 'sub1', <UserOutlined />, [
+	  getItem('Tom', '3'),
+	  getItem('Bill', '4'),
+	  getItem('Alex', '5'),
+	]),
+	getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+	getItem('Files', '9', <FileOutlined />),
+  ];
+  
+  
 	return (
-		<>
-			<div role="navigation">
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/page-2">Page 2</Link>
-					</li>
-				</ul>
+		<Layout style={{ minHeight: '100vh' }}>
+		<Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+		  <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
+		  <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+		</Sider>
+		<Layout className="site-layout">
+		  <Header style={{ padding: 0, background: colorBgContainer }} />
+		  <Content style={{ margin: '0 16px' }}>
+			<Breadcrumb style={{ margin: '16px 0' }}>
+			  <Breadcrumb.Item>User</Breadcrumb.Item>
+			  <Breadcrumb.Item>Bill</Breadcrumb.Item>
+			</Breadcrumb>
+			<div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
+			  Bill is a cat.
 			</div>
-			<Routes>
-				<Route
-					path="/"
-					element={
-						<div>
-							<Home />
-							<Link to="/page-2">Click here to go to page 2.</Link>
-						</div>
-					}
-				/>
-				<Route
-					path="/page-2"
-					element={
-						<div>
-							Page 2
-							<Link to="/">Click here to go back to root page.</Link>
-						</div>
-					}
-				/>
-			</Routes>
-		</>
+		  </Content>
+		  <Footer style={{ textAlign: 'center' }}>Ant Design ©2023 Created by Ant UED</Footer>
+		</Layout>
+	  </Layout>
 	);
-}
+  };
+
+// export function App() {
+// 	return (
+// 		<>
+// 			<Nav />
+// 			<div role="navigation">
+// 				<ul>
+// 					<li>
+// 						<Link to="/">Home</Link>
+// 					</li>
+// 					<li>
+// 						<Link to="/page-2">Page 2</Link>
+// 					</li>
+// 				</ul>
+// 			</div>
+// 			<Routes>
+// 				<Route
+// 					path="/"
+// 					element={
+// 						<div>
+// 							<Home />
+// 							<Link to="/page-2">Click here to go to page 2.</Link>
+// 						</div>
+// 					}
+// 				/>
+// 				<Route
+// 					path="/page-2"
+// 					element={
+// 						<div>
+// 							Page 2
+// 							<Link to="/">Click here to go back to root page.</Link>
+// 						</div>
+// 					}
+// 				/>
+// 			</Routes>
+// 		</>
+// 	);
+// }
 
 export default App;
